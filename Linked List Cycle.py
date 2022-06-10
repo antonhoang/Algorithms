856357a960ca6b2f938a6ea5e7c0b64e4414ee36
antuan.khoanh <antonhoang@Antons-MacBook-Pro.local>
2022-06-10 10:47:40 +0200

    Linked List Cycle

---
+++ Linked List Cycle.py
@@ -0,0 +1,22 @@
+from typing import List
+from typing import Optional
+
+# Definition for singly-linked list.
+
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+def hasCycle(self, head: Optional[ListNode]) -> bool:
+    if not head:
+        return False
+    slow = head
+    fast = head
+
+    while fast.next and fast.next.next:
+        fast = fast.next.next
+        slow = slow.next
+        if slow == fast:
+            return True
+    return False
